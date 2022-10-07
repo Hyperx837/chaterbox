bff483ba3dc92462b68535d34734e26b010bc770
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-10-07 20:40:10 +0530

    removed spirits to keep consistancy in avatars

--- backend/app/avatar.py
+++ backend/app/avatar.py
@@ -4,16 +4,7 @@ from urllib.parse import quote
 
 
 def random_avatar():
-    spirits = [
-        "male",
-        "female",
-        "human",
-        "jdenticon",
-        "avataaars",
-        "bottts",
-        "gridy",
-        "micah",
-    ]
+    spirits = ["micah", "miniavs"]
     seed = quote("".join(random.choice(string.printable) for _ in range(6)), safe="")
     url = f"https://avatars.dicebear.com/api/{random.choice(spirits)}/{seed}.svg"
     return url
